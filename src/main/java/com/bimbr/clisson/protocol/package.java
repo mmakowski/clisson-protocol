1029ac0c6eb250c2385a7742bdd2e80f899d5d35
Maciek Makowski <maciek.makowski@gmail.com>
2012-02-25 18:40:50 +0000

    added package doc (still to do)

---
+++ src/main/java/com/bimbr/clisson/protocol/package.java
@@ -0,0 +1,8 @@
+/**
+ * Contains classes used for communication between clients and the server.
+ * 
+ * TODO;
+ * 
+ * Date format: {@code yyyy-MM-dd HH:mm:ss.SSSZ}
+ */
+package com.bimbr.clisson.protocol;
